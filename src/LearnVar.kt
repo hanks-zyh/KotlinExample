f81ed5f87dae208201ca85cb2543e37f2c0f54f0
hanks-zyh <zhangyuhan2014@gmail.com>
2015-10-28 10:44:21 +0800

    add LearnVar

---
+++ src/LearnVar.kt
@@ -0,0 +1,16 @@
+/**
+ * Created by hanks on 15-10-28.
+ */
+fun main(args: Array<String>) {
+    val a = 1
+    val b: Int = 2
+    val c: Int
+    c = 1
+    println(a)
+    println(b)
+    println(c)
+
+    var x = 5
+    x += 1
+    println(x)
+}
