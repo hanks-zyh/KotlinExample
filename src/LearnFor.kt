85e721dddf80198c81eac02e6bcba0129e947ea6
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-10 11:36:22 +0800

    learn for

--- src/LearnFor.kt
+++ src/LearnFor.kt
@@ -1,6 +1,45 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray
+import java.util.*
+
 /**
  * Created by hanks on 15-10-28.
  */
 fun main(args: Array<String>) {
-//    var city  = [1,2,3];
+
+    var names = StringArray()
+    names.add("Bob")
+    names.add("Marry")
+    names.add("Fish")
+    names.add("Cat")
+    names.add("Dog")
+
+    for(i in 0..names.size-1){
+        print("${names[i]} " )
+    }
+    //Bob Marry Fish Cat Dog
+
+    println()
+
+    var cityList = ArrayList<String>()
+    cityList.add("beijing")
+    cityList.add("shanghai")
+    cityList.add("hangzhou")
+    cityList.add("shenzhen")
+
+    for(city in cityList){
+        print("$city ")
+    }
+    for(i in cityList.indices){
+        print("${cityList[i]} ")
+    }
+    //beijing shanghai hangzhou shenzhen
+
+
+    for((index, value) in cityList.withIndex()){
+        println("the element at $index is $value")
+    }
+    //    the element at 1 is shanghai
+    //    the element at 2 is hangzhou
+    //    the element at 3 is shenzhen
+
 }
