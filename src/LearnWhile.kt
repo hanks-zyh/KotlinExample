543cbfd3c22510803188a56fbf444ea5150ded31
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-10 11:39:36 +0800

    learn while

---
+++ src/LearnWhile.kt
@@ -0,0 +1,17 @@
+/**
+ * Created by hanks on 15-11-10.
+ */
+
+fun main(args: Array<String>) {
+
+    var x = 5
+    while(x>0){
+        x--
+    }
+
+    var y = -5
+    do{
+        y++
+    }while(y != 0)
+
+}
