1d7fcdc8403806049cfa93333b855aed54c8a0ee
hanks-zyh <zhangyuhan2014@gmail.com>
2016-03-15 23:33:32 +0800

    learn Companion

---
+++ src/LearnCompanion.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by hanks on 16/3/15.
+ */
+class Utils{
+    companion object{}
+}
+
+fun Utils.Companion.getName() = " Utils: $this.javaClass.name" //
+
+
+fun main(args: Array<String>) {
+    println(Utils.getName())
+}
