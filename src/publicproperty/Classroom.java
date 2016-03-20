afedb2be1658d98f4c3c84271fc72da65585f3ad
hanks-zyh <zhangyuhan2014@gmail.com>
2016-03-20 18:01:50 +0800

    use @JVMField

---
+++ src/publicproperty/Classroom.java
@@ -0,0 +1,11 @@
+package publicproperty;
+
+/**
+ * Created by hanks on 16/3/20.
+ */
+public class Classroom {
+
+    public void setName(Person person){
+        person.name = "hhh";
+    }
+}
---
+++ src/publicproperty/Person.kt
@@ -0,0 +1,8 @@
+package publicproperty
+
+/**
+ * Created by hanks on 16/3/20.
+ */
+class Person {
+    @JvmField    var name: String ? = null;
+}
